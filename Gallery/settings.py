2f7f471abc66291000f48c22ade2a4fd9badc437
Mohit Tr <mohit@Mohit.Mohit>
2021-07-16 17:29:34 +0530

    1st Deploy

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%)5-#x#av*)(_4ikxn*b2lgut3d%&k0+@4cfj)2((!5!r3%+2('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
