8b77be2ceb08c7cb74991b096b0961a303d15d64
Mohit Tr <mohit@Mohit.Mohit>
2021-07-16 22:26:53 +0530

    1st Deploy

--- Application/models.py
+++ Application/models.py
@@ -4,35 +4,35 @@ from django.db import models
 
 class Sketch(models.Model):
 	title = models.CharField(max_length = 100)
-	images = models.ImageField(upload_to = 'Application/Sketch')
+	images = models.ImageField(upload_to = 'Application\Sketch')
 	description = models.TextField()
 	def __str__(self):
 		return self.title
 	
 class Nature(models.Model):
 	title = models.CharField(max_length = 100)
-	images = models.ImageField(upload_to = 'Application/Nature')
+	images = models.ImageField(upload_to = 'Application\Nature')
 	description = models.TextField()
 	def __str__(self):
 		return self.title
 		
 class Portrait(models.Model):
 	title = models.CharField(max_length = 100)
-	images = models.ImageField(upload_to = 'Application/Portrait')
+	images = models.ImageField(upload_to = 'Application\Portrait')
 	description = models.TextField()
 	def __str__(self):
 		return self.title
 		
 class Cars(models.Model):
 	title = models.CharField(max_length = 100)
-	images = models.ImageField(upload_to = 'Application/Cars')
+	images = models.ImageField(upload_to = 'Application\Cars')
 	description = models.TextField()
 	def __str__(self):
 		return self.title
 		
 class Wildlife(models.Model):
 	title = models.CharField(max_length = 100)
-	images = models.ImageField(upload_to = 'Application/Wildlife')
+	images = models.ImageField(upload_to = 'Application\Wildlife')
 	description = models.TextField()
 	def __str__(self):
 		return self.title
--- Procfile
+++
@@ -1,2 +0,0 @@
-web: python manage.py collectstatic --no-input; gunicorn Gallery.wsgi --log-file - --log-level debug
-
